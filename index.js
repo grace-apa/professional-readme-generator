c2c5b1617e214fcd7816c69dc06443afdff37eca
grace-apa <149172807+grace-apa@users.noreply.github.com>
2024-02-05 14:00:08 +0000

    update to index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+// const fs = require("fs");
+// const path = require('path');
+const inquirer = require("inquirer");
+// const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+  },
+];
+
+inquirer.prompt(questions).then((data) => {
+  console.log(data);
+});
+
+// function to write README file
+function writeToFile(fileName, data) {}
+
+// function to initialize program
+function init() {}
+
+// function call to initialize program
+init();
