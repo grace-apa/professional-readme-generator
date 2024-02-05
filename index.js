060f47c7924670403fd1163d819a67d76ed9fb0f
grace-apa <149172807+grace-apa@users.noreply.github.com>
2024-02-05 14:21:11 +0000

    const fs added to index.js file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// const fs = require("fs");
 // const path = require('path');
 const inquirer = require("inquirer");
+const fs = require("fs");
 // const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
