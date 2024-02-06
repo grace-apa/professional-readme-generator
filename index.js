a3b358090f3e87a0bd431ae5d5541bdef08377f2
grace-apa <149172807+grace-apa@users.noreply.github.com>
2024-02-06 21:42:57 +0000

    error removed

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
-const path = require("path");
+// const path = require("path");
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
-// const generateMarkdown = require("./utils/generateMarkdown");
+// const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = () =>
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "fileName",
-      message: "What is the title of your project?",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Provide a brief description of your project?",
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: "How can users install your project?",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "How should users use your project?",
-    },
-    {
-      type: "list",
-      name: "license",
-      message: "Choose a license for your project:",
-      choices: ["MIT", "None"],
-    },
-    {
-      type: "input",
-      name: "contributing",
-      message: "How can others contribute to your project?",
-    },
-    {
-      type: "input",
-      name: "tests",
-      message: "Please provide instructions for running tests:",
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "What is your GitHub username?",
-    },
-  ]);
+// array of questions for user:
+const questions = [
+  {
+    type: "input",
+    name: "fileName",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a brief description of your project?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How can users install your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How should users use your project?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project:",
+    choices: ["MIT", "None"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "How can others contribute to your project?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Please provide instructions for running tests:",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+  },
+];
 
 const generateREADME = (answers) => {
   return `
@@ -101,10 +100,28 @@ const createMarkdownTemplate = (answers) => {
   return `# ${header}`;
 };
 
-inquirer.prompt(questions).then((data) => {
-  const { fileName, data, answers } = data;
-  const file = `${fileName}.${data}`;
-  let fileText;
+inquirer.prompt(questions).then((answers) => {
+  const { fileName } = answers;
+  const file = `${fileName}.md`;
+  const fileText = generateREADME(answers);
+});
+
+// mini project - Aarons version
+promptUser()
+  .then((answers) => fs.writeFileAsync(generateREADME(answers)))
+  .then(() => console.log("Successfulley generated README"))
+  .catch((err) => console.error(err));
+
+// another version:
+// promptUser()
+//   .then((answers) => {
+//     const readmeContent = generateREADME(answers);
+//     fs.writeFileSync("README.md", readmeContent);
+//     console.log("README generated successfully!");
+//   })
+//   .catch((error) => {
+//     console.error("Error occurred:", error);
+//   });
 
 //   switch (fileType) {
 //     case "html":
@@ -117,7 +134,7 @@ inquirer.prompt(questions).then((data) => {
 //     default:
 //       fileText = "Oops, this file type isn't implemented yet";
 //       break;
-  }
+//   }
 
 //   // function to write README file
 //   fs.writeFile(file, fileText, (err) =>
@@ -126,11 +143,11 @@ inquirer.prompt(questions).then((data) => {
 // });
 
 // function to write README file
-function writeToFile (fileName, data) {
-  fs.writeFile(fileName, data, (err) =>
-    err ? console.error(err) : console.log("README generated successfully!")
-  );
-});
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    err ? console.error("err") : console.log("README generated successfully!");
+  });
+}
 
 // // function to write README file
 // function writeToFile(fileName, data) {}
